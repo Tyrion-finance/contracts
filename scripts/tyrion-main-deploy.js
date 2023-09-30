c563c310c24f6dbfaf6a6bcc332aec89eb095b14
Wojciech Sobczuk <wojtek@esvelo.com>
2023-09-30 17:38:03 +0200

    deployment fix

--- scripts/tyrion-main-deploy.js
+++ scripts/tyrion-main-deploy.js
@@ -31,18 +31,18 @@ function getUserInput(promptText) {
 async function mainETH() {
     const Tyrion = await hre.ethers.getContractFactory('Tyrion');
     const tyrion = await Tyrion.deploy(ethers.constants.AddressZero);
+    console.log("Tyrion", tyrion.address);
 
     const TyrionRegistry = await hre.ethers.getContractFactory('TyrionRegistry');
     const tyrionRegistry = await TyrionRegistry.deploy();
+    console.log("TyrionRegistry", tyrionRegistry.address);
 
     const TyrionBroker = await hre.ethers.getContractFactory('TyrionBroker');
     const tyrionBroker = await TyrionBroker.deploy(tyrion.address, tyrionRegistry.address);
+    console.log("TyrionBroker", tyrionBroker.address);
 
     const [owner, user1, user2, user3] = await ethers.getSigners();
 
-    await tyrionRegistry.registerReferrer(ethers.constants.AddressZero);
-
-    console.log("all deployed?");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
