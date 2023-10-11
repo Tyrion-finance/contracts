3ec9ce50c718549e359e04294e2e89f639c294ed
Wojciech Sobczuk <wojtek@esvelo.com>
2023-10-11 09:19:22 +0200

    fixed TVG-04M and TVG-04S

--- contracts/TyrionVesting.sol
+++ contracts/TyrionVesting.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
@@ -59,7 +60,7 @@ contract TyrionVesting is Ownable, ReentrancyGuard {
         require(_amount > 0, "Amount should be 0");
         require(_startTime >= block.timestamp - 30 days, "Start time shouldn't be before 30 days in the past");
 
-        SafeERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+        SafeERC20.safeTransferFrom(IERC20(_token), msg.sender, address(this), _amount);
 
         newVestingId = vestingIdCounter.current();
         vestingIdCounter.increment();
@@ -100,7 +101,7 @@ contract TyrionVesting is Ownable, ReentrancyGuard {
 
         emit Withdrawn(vesting.id, msg.sender, toWithdraw);
 
-        SafeERC20(vesting.token).safeTransfer(msg.sender, toWithdraw);
+        SafeERC20.safeTransfer(IERC20(vesting.token), msg.sender, toWithdraw);
     }
 
     function withdrawableAmount(uint256 vestingId) public view returns (uint256) {
