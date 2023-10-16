645ae820cb9226297027276e96bc9e5a1a600bc8
Wojciech Sobczuk <wojtek@esvelo.com>
2023-10-16 17:16:22 +0200

    script fixes

--- scripts/tyrion-main-deploy-old.js
+++ scripts/tyrion-main-deploy-old.js
@@ -5,23 +5,23 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
-const {deployUpgradable} = require("./utils");
 
 
 async function mainETH() {
-    const tyrion = {
-        address: '0x881020118fb2A40B81D819eCD32C9A417d914aCA'
-    }
+    const tyrion_address = '0x5e27e384aCBBa20982f991893B9970AaF3f43181';
 
-    const tyrionRegistry = await deployUpgradable('TyrionRegistry', []);
+    const TyrionRegistry = await hre.ethers.getContractFactory('TyrionRegistry');
+    const tyrionRegistry = await TyrionRegistry.deploy();
     console.log("TyrionRegistry", tyrionRegistry.address);
-    console.log("Implementation", await tyrionRegistry.implementation())
 
-    const tyrionBroker = await deployUpgradable('TyrionBroker', [tyrion.address, tyrionRegistry.address]);
+    const TyrionBroker = await hre.ethers.getContractFactory('TyrionBroker');
+    const tyrionBroker = await TyrionBroker.deploy(tyrion_address, tyrionRegistry.address);
     console.log("TyrionBroker", tyrionBroker.address);
-    console.log("Implementation", await tyrionBroker.implementation())
 
     await tyrionRegistry.setBrokerAddress(tyrionBroker.address);
+
+    const [owner, user1, user2, user3] = await ethers.getSigners();
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
